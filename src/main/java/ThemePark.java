73f018269da31051d45ea006797285097fad81ce
garyb6 <gary.bennett6@outlook.com>
2022-02-14 22:52:50 +0000

    set up ThemePark extension

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,29 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import people.Visitor;
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<behaviours.IReviewed> attractionsAndStalls;
+
+    public ThemePark() {
+        this.attractionsAndStalls = new ArrayList<IReviewed>(){
+
+        }
+    }
+
+    public static ArrayList<IReviewed> getAttractionsAndStalls() {
+        return attractionsAndStalls;
+    }
+
+    public ArrayList getAllReviewed() {
+
+    }
+
+    public void visit(Visitor visitor, Attraction attraction) {
+    }
+
+    public ArrayList getAllAllowedFor() {
+    }
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,40 @@
+import behaviours.IReviewed;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    private ThemePark themepark;
+
+    @Before
+    public void setUp(){
+        themepark = new ThemePark();
+    }
+
+    @Test
+    public void canGetList(){
+        assertEquals(, themepark.getAttractionsAndStalls());
+    }
+
+    @Test
+    public void canGetAllReviewed(){
+        assertEquals(, themepark.getAllReviewed());
+    }
+
+    @Test
+    public void canVisit(){
+        assertEquals(, themepark.visit());
+    }
+
+    @Test
+    public void cangetAllAllowedFor(){
+        assertEquals(, themepark.getAllAllowedFor());
+    }
+
+
+
+}
